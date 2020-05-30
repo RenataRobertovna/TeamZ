23424f0687ca66eb15c7435819b71cdbc36dd792
YakovGrechkin2003 <58858243+YakovGrechkin2003@users.noreply.github.com>
2020-05-30 14:00:10 +0300

    from DOS to DirectoryInfo
    перенос кода из main  в  DirectoryInfo

--- DOS_Z/DOS_Z/DOS.cs
+++ DOS_Z/DOS_Z/DOS.cs
@@ -9,19 +9,6 @@ namespace DOS_Z
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> catalogs = new Dictionary<int, string>();
-            DriveInfo[] drivers = DriveInfo.GetDrives();     // получаем диски
-            int i = 1;
-            foreach (var driver in drivers)                  // проходимся по дискам
-            {
-                if (driver.IsReady)
-                {
-                    //Для выбора к какому диску перейти
-                    WriteLine($"{i}. - { driver.Name }");
-                    catalogs.Add(i, driver.RootDirectory.ToString()); // добавляем в дикшионари порядковый номер диска и его путь
-                    i++;
-                }
-            }
             WriteLine("Введите номер диска");
             int numberRoot = Convert.ToInt32(ReadLine());
 
--- DOS_Z/DOS_Z/DirectoryInfo.cs
+++ DOS_Z/DOS_Z/DirectoryInfo.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using static System.Console;
 
 namespace DOS_Z
 {
     class DirectoryInfo
     {
+        private Dictionary<int, string> catalogs;
+        private Dictionary<int, string> files;
+
+        private DriveInfo[] drivers;
+
+        public DirectoryInfo()
+        {
+            drivers = DriveInfo.GetDrives();     // получаем диски
+            int i = 1;
+            foreach (var driver in drivers)                  // проходимся по дискам
+            {
+                if (driver.IsReady)
+                {
+                    //Для выбора к какому диску перейти
+                    WriteLine($"{i}. - { driver.Name }");
+                    catalogs.Add(i, driver.RootDirectory.ToString()); // добавляем в дикшионари порядковый номер диска и его путь
+                    i++;
+                }
+            }
+        }
+
     }
 }
+
