807c6d79054047c461b7eacabe05e5233ceb566a
Vladimir <58857922+Vladimir233@users.noreply.github.com>
2020-06-02 18:15:25 +0300

    Added comand delete
    Написал метод удаления папки

--- DOS_Z/DirectoryInformation.cs
+++ DOS_Z/DirectoryInformation.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using static System.Console;
@@ -12,11 +11,15 @@ namespace DOSz
         private List<string> catalogsName;
         private List<string> filesName;
         private List<string> message_cash;
+        private List<string> greenNames;
+        private string CopiedPath;
+        private string CopiedName;
         private string CurrentPath; // текущий путь
         private readonly string RootPath; // самый старший путь
         private DriveInfo[] drivers; // список устройств
         public DirectoryInformation()
         {
+            greenNames = new List<string>();
             message_cash = new List<string>();
             catalogs = new List<string>();
             files = new List<string>();
@@ -28,6 +31,7 @@ namespace DOSz
         }
         public void ShowCurrentCatalogs() //показ каталога
         {
+            string previousPath = CurrentPath;
             Console.Clear();
             Write($"Текущая директория: ---> ");
             Console.ForegroundColor = ConsoleColor.Magenta;
@@ -38,39 +42,94 @@ namespace DOSz
             WriteLine($"{0}. - НАЗАД");
             Console.ResetColor();
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            WriteLine("--------------Каталоги--------------");
+            WriteLine("\t\t{0}\t\t\t\t\t{1}", "Каталоги", "Файлы");
             WriteLine();
+            for (int i = 0; i < catalogsName.Count || i < filesName.Count; i++)
+            {
+                string folderName = "";
+                string fileName = "";
+                bool fold = false;
+                bool file = false;
+                if (i < catalogsName.Count)
+                {
+                    fold = true;
+                    folderName = catalogsName[i];
+                    if (folderName.Length >= 20)
+                    {
+                        folderName = folderName.Substring(0, 20);
+                    }
+                }
+                if (i < filesName.Count)
+                {
+                    file = true;
+                    fileName = filesName[i];
+                    if (fileName.Length >= 20)
+                    {
+                        fileName = fileName.Substring(0, 20);
+                    }
+                }
+                if (fold && file)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Write("{0, -5}", (i + 1).ToString() + ".");
+                    if (greenNames.Contains(folderName))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    Write("{0, -20}\t\t\t", folderName);
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Write("{0, -5}", (i + 1).ToString() + ".");
+                    if (greenNames.Contains(fileName))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    WriteLine("{0, -20}", fileName);
+                }
+                else if (fold && !file)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Write("{0, -5}", (i + 1).ToString() + ".");
+                    if (greenNames.Contains(folderName))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    WriteLine("{0, -20}\t\t\t", folderName);
+                }
+                else if (!fold && file)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Write("{0, -5}", (i + 1).ToString() + ".");
+                    if (greenNames.Contains(fileName))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                    }
+                    WriteLine("{0, -20}", fileName);
+                }
+            }
             Console.ForegroundColor = ConsoleColor.Cyan;
-            for (int i = 0; i < catalogsName.Count; i++)
-            {
-                WriteLine($"{i + 1}. - {catalogsName[i]}");
-            }
-            //                    ТО ЖЕ САМОЕ, ЧТО И СНИЗУ
-            /*
-            foreach (KeyValuePair<int, string> keyValue in catalogsName)
-            {
-                WriteLine($"{keyValue.Key}. - {keyValue.Value}");
-            }
-            */
             WriteLine();
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            WriteLine("--------------Файлы--------------");
-            WriteLine();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            for (int i = 0; i < filesName.Count; i++)
-            {
-                WriteLine($"{i + 1}. - {filesName[i]}");
-            }
-            WriteLine();
-
-            for(int i = 0; i < message_cash.Count; i++)
+            for (int i = 0; i < message_cash.Count; i++)
             {
                 WriteLine(message_cash[i]);
-
             }
             message_cash.Clear();
             DirectoryInfo dirInfo = new DirectoryInfo(CurrentPath);
-
             while (true)
             {
                 message_cash.Add("Жду команду");
@@ -99,7 +158,7 @@ namespace DOSz
                         }
                     }
                     message_cash.Clear();
-                    Directories();
+                    Directories(previousPath);
                     break;
                 }
                 catch (System.FormatException)
@@ -115,7 +174,8 @@ namespace DOSz
                         fs.Close();
                         WriteLine("Файл создан.");
                         message_cash.Add("Файл создан.");
-                        Directories();
+                        greenNames.Add(myFile.Name);
+                        Directories(previousPath);
                         return;
                     }
                     else if (command.Contains("folder"))
@@ -127,9 +187,10 @@ namespace DOSz
                             dirInfo1.CreateSubdirectory(@nameNewFolder);
                             WriteLine("Папка создана.");
                             message_cash.Add("Папка создана.");
+                            greenNames.Add(new DirectoryInfo(CurrentPath + @"\" + nameNewFolder).Name);
+                            Directories(previousPath);
+                            return;
                         }
-                        Directories();
-                        return;
                     }
                     else
                     {
@@ -142,24 +203,106 @@ namespace DOSz
                     WriteLine("==========Подсказка==============\n" +
                               "create file/folder <наз-е файла> \n" +
                               "delete file/folder <номер> \n" +
-                              "copy file/folder <номер>  \n" +
+                              "move   file/folder <номер> \n" +
+                              "copy   file        <номер> \n" +
+                              "insert - вставить file/folder \n" +
                               "Для перехода на след каталог\n" +
-                              "нужно ввести номер каталога. \n" +
+                              "нужно ввести номер каталога.\n" +
                               "(0 - перейти в родительский каталог)\n" +
                               "=================================");
                 }
+                else if (command.Contains("move"))
+                {
+                    if (command.Contains("folder"))
+                    {
+                        int numberFolder = Convert.ToInt32(command.Substring(12));
+                        if (catalogs[numberFolder - 1] != null)
+                        {
+                            CopiedPath = catalogs[numberFolder - 1];
+                            CopiedName = catalogsName[numberFolder - 1];
+                        }
+                    }
+                    else if (command.Contains("file"))
+                    {
+                        int numberFile = Convert.ToInt32(command.Substring(10));
+                        if (files[numberFile - 1] != null)
+                        {
+                            CopiedPath = files[numberFile - 1];
+                            CopiedName = filesName[numberFile - 1];
+                        }
+                    }
+                }
+                else if (command.Contains("insert"))
+                {
+                    FileInfo fileInf = new FileInfo(CopiedPath);
+                    if (fileInf.Exists)
+                    {
+                        fileInf.MoveTo(CurrentPath + @"\" + CopiedName);
+                        WriteLine("Файл перемещен успешно.");
+                    }
+                    else
+                    {
+                        DirectoryInfo dirInfo1 = new DirectoryInfo(CopiedPath);
+                        if (dirInfo1.Exists && Directory.Exists(CurrentPath + @"\" + CopiedName) == false)
+                        {
+                            dirInfo1.MoveTo(CurrentPath + @"\" + CopiedName);
+                            WriteLine("Папка перемещена успешно.");
+                        }
+                    }
+                }
+                   else if (command.Contains("delete"))
+                {
+                    if (command.Contains("file"))
+                    {
+                        // command = delete file 5444
+                        int numberFile = Convert.ToInt32(command.Substring(12));
+                        if (files[numberFile-1] != null)
+                        {
+                            FileInfo fileInf = new FileInfo(files[numberFile-1]);
+                            if (fileInf.Exists)
+                            {
+                                fileInf.Delete();
+                                WriteLine("Файл удален успешно!");
+                                message_cash.Add("Файл удален успешно!");
+                                Directories(previousPath);
+                                return;
+                            }
+                        }
+                    }
+                    else if (command.Contains("folder"))
+                    {
+                        int numberFolder = Convert.ToInt32(command.Substring(14));
+                        if(catalogs[numberFolder-1] != null)
+                        {
+                            try
+                            {
+                                DirectoryInfo dirInfo1 = new DirectoryInfo(catalogs[numberFolder - 1]);
+                                dirInfo1.Delete(true);
+                                WriteLine("Каталог удален");
+                                message_cash.Add("Каталог удален");
+                                Directories(previousPath);
+                                return;
+
+                            }
+                            catch (Exception ex)
+                            {
+                                WriteLine(ex.Message);
+                            }
+                        }
+                    }
+                }
             }
         }
-        //показ файлов
-        private void ShowFilesInCatalog() { }
-        //переход в следующий каталог
-        public void ChangeCurrentCatalog() { }
-        private void Directories()
+        private void Directories(string previousPath)
         {
             try
             {
                 if (!CurrentPath.Equals(RootPath))
                 {
+                    if (!CurrentPath.Equals(previousPath))
+                    {
+                        greenNames.Clear();
+                    }
                     ClearLists();
                     string[] dirs = Directory.GetDirectories(CurrentPath);
                     string[] files = Directory.GetFiles(CurrentPath);
